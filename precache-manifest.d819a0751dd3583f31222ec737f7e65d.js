7a5b0168914669efff6c43bd8df72ed42ba6095b
Tristan Lannigan <tristanlannigan@gmail.com>
2020-02-20 12:54:42 -0700

    Updates

---
+++ precache-manifest.d819a0751dd3583f31222ec737f7e65d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f5ac4bbd82a182e1431026edd48e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57839c212775c5dd2465",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9fa82d5e0857fa6536f1",
+    "url": "/static/js/2.82137c95.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.82137c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57839c212775c5dd2465",
+    "url": "/static/js/main.66bc1f4d.chunk.js"
+  },
+  {
+    "revision": "e6b676ceb15709458cbb",
+    "url": "/static/js/runtime-main.d0bb43e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
